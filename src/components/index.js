b21d769fb3f398f4fd9ac83ecf4a836693e14d1e
Amit Joshi <amit.joshi314@gmail.com>
2025-02-09 17:26:45 +0530

    Production grade react components

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,14 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import Container from "./Container/Container";
+import Logo from "./Logo";
+import Logout from './Header/Logout'
 
 
 export {
     Footer,
-    Header
+    Header,
+    Container,
+    Logo,
+    Logout
 }
